a6979558e0617f7f97f8c5b251bf0742591e5584
Shashi <shashiwadananirmani14@gmail.com>
2020-12-03 00:02:52 +0530

    graphql server connected

--- server/server.js
+++ server/server.js
@@ -3,6 +3,15 @@ const typeDefinition = `
       greeting: String
    }`
 
+ const  resolverObject = {
+   Query : {
+      greeting: () => 'Hello !!'
+   }
+}
+
+const {makeExecutableSchema} = require('graphql-tools')
+const schema = makeExecutableSchema({typeDefs:typeDefinition, resolvers:resolverObject})
+const {graphqlExpress, graphiqlExpress} = require('apollo-server-express')
 const bodyParser = require('body-parser')
    const cors = require('cors')
    const express = require('express')
@@ -12,4 +21,7 @@ const bodyParser = require('body-parser')
    
    //register middleware
    app.use(bodyParser.json() , cors())
+   app.use('/graphql',graphqlExpress({schema}))
+   app.use('/graphiql',graphiqlExpress({endpointURL:'/graphql'}))
    app.listen(port, () =>  console.log(`server is up and running at ${port}`))
+   
